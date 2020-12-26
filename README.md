55d9baf95cf62e5ab4edacf97d02b36016b9238b
kryz <k@kryz.dev>
2020-12-26 11:21:58 +0100

    first program

---
+++ README.md
@@ -0,0 +1,14 @@
+### Based on Udemy course:
+
+[Learn Assembly Language by Making Games for the Atari 2600](https://www.udemy.com/course/programming-games-for-the-atari-2600/)
+
+**Used tools:**
+
+* [DASM](https://dasm-assembler.github.io/)
+* [Stella](https://stella-emu.github.io/)
+
+**Run example:**
+
+```shell script
+./dasm src/cleanmem/cleanmem.asm -f3 -v0 -oout/cleanmem.bin
+```
---
+++ src/cleanmem/cleanmem.asm
@@ -0,0 +1,24 @@
+    processor 6502
+
+    seg code
+    org $F000       ; define the code origin at $F000
+
+start:
+
+    sei             ; disable interrupts
+    cld             ; clear the BCD decimal math mode
+    ldx #$FF        ; load the x register with #$FF
+    txs             ; transfer the x register to the stack pointer
+
+    lda #0          ; A = 0
+    ldx #$FF        ; X = #$FF
+
+clearmem:
+
+    sta $0,X        ; store the value of A inside memory address $0 + X
+    dex             ; X--
+    bne clearmem    ; loop until x is equal to 0 (z-flag is set)
+
+    org $FFFC
+    .word start
+    .word start
